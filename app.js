64356db2d9080c0ca1fd00ff2bcbee66fcd1ae4c
mafromist <mafromist@gmail.com>
2021-08-10 23:15:59 +0100

    CSS Framework and a new feature added
    Bulma CSS added and also results are clearing out after submission new query

---
+++ app.js
@@ -0,0 +1,27 @@
+const form = document.querySelector("#form");
+const searchArea = document.querySelector(".searchArea");
+const resultsArea = document.querySelector(".resultsArea");
+
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const searchTerm = form.elements.query.value;
+  resultsArea.innerHTML = "";
+  try {
+    const config = { params: { q: searchTerm } };
+    const res = await axios.get(`https://api.tvmaze.com/search/shows`, config);
+    makeImages(res.data);
+    form.elements.query.value = "";
+  } catch (err) {
+    console.log("Error: ", err);
+  }
+});
+
+const makeImages = (shows) => {
+  for (let result of shows) {
+    if (result.show.image) {
+      const showImage = document.createElement("img");
+      showImage.src = result.show.image.medium;
+      resultsArea.append(showImage);
+    }
+  }
+};
